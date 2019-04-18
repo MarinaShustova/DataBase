d015f132414096b7726221fed7013b53188a2978
Марина Шустова <mshustovam@gmail.com>
2019-04-18 22:10:52 +0700

    удалила файлы

--- src/main/kotlin/theater/Student.kt
+++
@@ -1,4 +0,0 @@
-package theater
-
-
-data class Student(val id: Long?, val name: String, var group: Group)
--- src/main/resources/db/migration/V2__Create_contains.sql
+++
@@ -1,4 +0,0 @@
-CREATE SEQUENCE contains_sqs START 1;
-CREATE TABLE contains(
-id        bigint PRIMARY KEY DEFAULT NEXTVAL('contains_sqs')
-);
