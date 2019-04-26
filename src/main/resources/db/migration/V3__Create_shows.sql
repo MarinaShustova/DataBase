ab07c8a5e1ff815d0e244fdb23d41488f8ab323b
avoloshina <a.voloshina@g.nsu.ru>
2019-04-26 17:13:32 +0700

    delete migrations

--- src/main/resources/db/migration/V3__Create_shows.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE shows
-(
-  id          serial PRIMARY KEY,
-  show_date   timestamp NOT NULL,
-  premiere    boolean   NOT NULL,
-  performance integer REFERENCES performance (id) ON DELETE CASCADE
-);
