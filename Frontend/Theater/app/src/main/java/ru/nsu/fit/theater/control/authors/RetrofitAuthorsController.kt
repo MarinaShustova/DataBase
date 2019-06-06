19834cf535600abe5584ca8fd86b779c7c245915
Ivan Molochev <molo4evan@gmail.com>
2019-06-06 23:58:12 +0700

    Added some more controller implementations

--- Frontend/Theater/app/src/main/java/ru/nsu/fit/theater/control/authors/RetrofitAuthorsController.kt
+++ Frontend/Theater/app/src/main/java/ru/nsu/fit/theater/control/authors/RetrofitAuthorsController.kt
@@ -1,33 +1,113 @@
 package ru.nsu.fit.theater.control.authors
 
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import ru.nsu.fit.theater.App
 import ru.nsu.fit.theater.retrofit.model.AuthorData
 
 class RetrofitAuthorsController: IAuthorsController {
     override fun createAuthor(data: AuthorData, callback: IAuthorsController.ICreateAuthorCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        App.api.createAuthor(data).enqueue(object : Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                callback.onError()
+            }
+
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.isSuccessful) callback.onAuthorCreated()
+                else callback.onError()
+            }
+        })
     }
 
     override fun getAuthor(id: Int, callback: IAuthorsController.IGetAuthorCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        App.api.getAuthorById(id).enqueue(object : Callback<AuthorData> {
+            override fun onFailure(call: Call<AuthorData>, t: Throwable) {
+                callback.onError()
+            }
+
+            override fun onResponse(call: Call<AuthorData>, response: Response<AuthorData>) {
+                if (!response.isSuccessful || response.body() == null){
+                    callback.onError()
+                } else {
+                    callback.onAuthorLoaded(response.body()!!)
+                }
+            }
+        })
     }
 
     override fun getAuhtors(callback: IAuthorsController.IGetAuthorsCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        App.api.getAuthors().enqueue(object : Callback<List<AuthorData>> {
+            override fun onFailure(call: Call<List<AuthorData>>, t: Throwable) {
+                callback.onError()
+            }
+
+            override fun onResponse(call: Call<List<AuthorData>>, response: Response<List<AuthorData>>) {
+                if (!response.isSuccessful || response.body() == null){
+                    callback.onError()
+                } else {
+                    callback.onAuthorsLoaded(response.body()!!)
+                }
+            }
+        })
     }
 
     override fun getAuthorsOfCountry(name: String, callback: IAuthorsController.IGetAuthorsOfCountryCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        App.api.getAuthorsOfCountry(name).enqueue(object : Callback<List<AuthorData>> {
+            override fun onFailure(call: Call<List<AuthorData>>, t: Throwable) {
+                callback.onError()
+            }
+
+            override fun onResponse(call: Call<List<AuthorData>>, response: Response<List<AuthorData>>) {
+                if (!response.isSuccessful || response.body() == null){
+                    callback.onError()
+                } else {
+                    callback.onAuthorsLoaded(response.body()!!)
+                }
+            }
+        })
     }
 
     override fun getAuthorsOfCentury(century: Int, callback: IAuthorsController.IGetAuthorsOfCenturyCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        App.api.getAuthorsOfCentury(century).enqueue(object : Callback<List<AuthorData>> {
+            override fun onFailure(call: Call<List<AuthorData>>, t: Throwable) {
+                callback.onError()
+            }
+
+            override fun onResponse(call: Call<List<AuthorData>>, response: Response<List<AuthorData>>) {
+                if (!response.isSuccessful || response.body() == null){
+                    callback.onError()
+                } else {
+                    callback.onAuthorsLoaded(response.body()!!)
+                }
+            }
+        })
     }
 
     override fun updateAuthor(id: Int, data: AuthorData, callback: IAuthorsController.IUpdateAuthorCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        App.api.updateAuthor(id, data).enqueue(object : Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                callback.onError()
+            }
+
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.isSuccessful) callback.onAuthorUpdated()
+                else callback.onError()
+            }
+        })
     }
 
     override fun deleteAuthor(id: Int, callback: IAuthorsController.IDeleteAuthorCallback) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        App.api.deleteAuthor(id).enqueue(object : Callback<ResponseBody> {
+            override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
+                callback.onError()
+            }
+
+            override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
+                if (response.isSuccessful) callback.onAuthorDeleted()
+                else callback.onError()
+            }
+        })
     }
 }
