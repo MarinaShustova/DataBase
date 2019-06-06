881a23858a00dc960df2a31428d3c6d74b36f76e
Марина Шустова <mshustovam@gmail.com>
2019-06-06 23:28:49 +0700

    первые шаги в андроиде

---
+++ Frontend/Theater/app/src/main/java/ru/nsu/fit/theater/CreateSpectacleActivity.kt
@@ -0,0 +1,12 @@
+package ru.nsu.fit.theater
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateSpectacleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_spectacle)
+    }
+}
