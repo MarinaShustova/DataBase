632efdc02e067d84ceff05452c19484318b8a3b4
Ivan Molochev <molo4evan@gmail.com>
2019-06-07 17:15:20 +0700

    Fixed merge bugs

--- Frontend/Theater/app/src/main/java/ru/nsu/fit/theater/controller/PlaybillController.kt
+++ Frontend/Theater/app/src/main/java/ru/nsu/fit/theater/controller/PlaybillController.kt
@@ -4,28 +4,22 @@ import ru.nsu.fit.theater.model.PlaybillItem
 import java.util.*
 
 class PlaybillController(){
-<<<<<<< HEAD
 //    private val baseProductsList = ArrayList<PlaybillItem>().apply {
-=======
     private val baseProductsList = ArrayList<PlaybillItem>().apply {
->>>>>>> 662c24c4e5814263c1efaa62de21f72a1888a042
 //        add(PlaybillItem(Date(2019, 6, 8, 13, 0), true, "Красная шапочка", 0))
 //        add(PlaybillItem(Date(2019, 6, 28, 13, 0), true, "Летучая мышь", 6))
 //        add(PlaybillItem(Date(2019, 6, 16, 13, 0), false, "Бал в честь Жака", 12))
 //        add(PlaybillItem(Date(2019, 6, 14, 13, 0), true, "Оловянный солдатик", 0))
-<<<<<<< HEAD
 //    }
 //
 //    fun getSpectacles(): ArrayList<PlaybillItem>{
 //        println(baseProductsList)
 //        return baseProductsList
 //    }
-=======
     }
 
     fun getSpectacles(): ArrayList<PlaybillItem>{
         println(baseProductsList)
         return baseProductsList
     }
->>>>>>> 662c24c4e5814263c1efaa62de21f72a1888a042
 }
